f5dbb4bae7b403fefbedb5a2efb3be70281afeb4
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-11-14 00:08:02 -0600

    Vista crear usuario

--- Proyecto1ED2/Controllers/HomeController.cs
+++ Proyecto1ED2/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace Proyecto1ED2.Controllers
 
             return View();
         }
+
+        public ActionResult CrearUsuario()
+        {
+            return View();
+        }
     }
 }
