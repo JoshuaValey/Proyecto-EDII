b4400f538869ab3973f8382c51f25d0ea041ba69
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-11-17 01:24:48 -0600

    Borrar apis que no se usan

--- APIMensajeria/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace APIMensajeria.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- APIMensajeria/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace APIMensajeria.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
