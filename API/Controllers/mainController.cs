93097773136abfac15487b98cab1f9b2e486c096
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-11-23 13:48:56 -0600

    Correcion de biblioteca de clases

--- API/Controllers/mainController.cs
+++ API/Controllers/mainController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Modelos;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,19 @@ namespace API.Controllers
             }
             //hacer comprobacion de usuario y contrase;a 
             //devolver si se comprobo o no 
+        }
+
+        public IActionResult NuevoUsuario(users usuario)
+        {
+            try
+            {
+
+                return StatusCode(200);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
