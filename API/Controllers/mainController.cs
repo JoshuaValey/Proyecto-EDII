22a3fdb81c76a6d26065de591c0729f5be9694b2
JoshuaValey <j97valey@gmail.com>
2020-11-23 15:10:17 -0600

    Consulta usuario login

--- API/Controllers/mainController.cs
+++ API/Controllers/mainController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using API.Connection;
+using API.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +22,13 @@ namespace API.Controllers
         [Route("Login/{user}/{password}")]
         public IActionResult Login(string user, string password)
         {
-            if (user == "marce")
+            DbConnection connection = new DbConnection();
+            var db = connection.Client.GetDatabase(connection.DBName);
+            var usersCollection = db.GetCollection<Usuario>("users");
+            var filter = Builders<Usuario>.Filter.Eq("user", user);
+            List<Usuario> usuariosLog = usersCollection.Find<Usuario>(filter).ToList();
+            Usuario userLog = UserLog(usuariosLog, user, password);
+            if (userLog != null)
             {
                 return StatusCode(200);
             }
@@ -26,6 +38,25 @@ namespace API.Controllers
             }
             //hacer comprobacion de usuario y contrase;a 
             //devolver si se comprobo o no 
+        }
+
+        static Usuario UserLog(List<Usuario> usuariosLog, string user, string pass)
+        {
+            Usuario retorno = null;
+            int i; bool exist = false;
+            for (i = 0; i < usuariosLog.Count; i++)
+            {
+                if (usuariosLog[i].Password == pass && usuariosLog[i].User == user)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist)
+            {
+                retorno = usuariosLog[i];
+            }
+            return retorno;
         }
     }
 }
---
+++ API/Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace API.Models
+{
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+        [BsonElement("apellido")]
+        public string Apellido { get; set; }
+        [BsonElement("password")]
+        public string Password { get; set; }
+        [BsonElement("user")]
+        public string User { get; set; }
+        [BsonElement("email")]
+        public string EMail { get; set; }
+        [BsonElement("llaveSdes")]
+        public string LlaveSDES { get; set; }
+        [BsonElement("numeroPrivado")]
+        public int NumeroPrivado { get; set; }
+
+        [BsonElement("contactos")]
+        public List<string> Contactos = new List<string>();
+
+        [BsonElement("guid")]
+        public string _Guid { get; set; }
+
+        public Usuario()
+        {
+            this._Guid = System.Guid.NewGuid().ToString();
+        }
+    }
+}
